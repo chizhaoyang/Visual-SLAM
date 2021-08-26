5bbc7101a63b5263b887e3f511e1626e3bddbaa2
Chizhao Yang <cy0003@mix.wvu.edu>
2021-08-25 23:06:24 -0400

    add bundle adjustment

--- Writing_Visual_SLAM_from_Scratch_0/boundle_adjustment/SnavelyReprojectionError.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include "ceres/ceres.h"
-#include "rotation.h"
-
-class SnavelyReprojectionError {
-    
-}
---
+++ Writing_Visual_SLAM_from_Scratch_0/bundle_adjustment/bundle_adjustment_ceres.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <ceres/ceres.h>
+#include "common.h"
+#include "SnavelyReprojectionError.h"
+
