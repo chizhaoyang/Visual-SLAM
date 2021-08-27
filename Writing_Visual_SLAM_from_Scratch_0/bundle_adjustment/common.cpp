cecdeb0788938ca363f87d83e6882496caf1f20e
Chizhao Yang <cy0003@mix.wvu.edu>
2021-08-26 22:57:10 -0400

    update

---
+++ Writing_Visual_SLAM_from_Scratch_0/bundle_adjustment/common.cpp
@@ -0,0 +1,216 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#include "common.h"
+#include "rotation.h"
+#include "random.h"
+
+using VectorRef = Eigen::Map<Eigen::VectorXd>;
+using ConstVectorRef = Eigen::Map<const Eigen::VectorXd>;
+
+template<typename T>
+void FscanfOrDie (FILE *fptr, const char *format, T *value) {
+    int num_scanned = fscanf(fptr, format, value);
+    if (num_scanned != 1) {
+        std::cerr << "Invalid UW data file. ";
+    }
+}
+
+void PerturbPoint3(const double sigma, double *point) {
+    for (int i = 0; i < 3; ++i) {
+        point[i] += RandNormal() * sigma;
+    }
+}
+
+double Median(std::vector<double> *data) {
+    int n = data->size();
+    std::vector<double>::iterator mid_point = data->begin() + n / 2;
+    std::nth_element(data->begin(), mid_point, data->end());
+    return *mid_point;
+}
+
+BALProblem::BALProblem(const std::string &filename, bool use_quaternions) {
+    FILE *fptr = fopen(filename.c_str(), "r");
+
+    if (fptr == NULL) {
+        std::cerr << "Error: unable to open file " << filename;
+        return;
+    }
+
+    // This will die horribly on invalid files. Them's the breaks.
+    FscanfOrDie(fptr, "%d", &num_cameras_);
+    FscanfOrDie(fptr, "%d", &num_points_);
+    FscanfOrDie(fptr, "%d", &num_observations_);
+
+    std::cout << "Header: " << num_cameras_ 
+              << " " << num_points_
+              << " " << num_observations_;
+
+    point_index_ = new int[num_observations_];
+    camera_index_ = new int[num_observations_];
+    observations_ = new double[2 * num_observations_];
+
+    num_parameters_ = 9 * num_cameras_ + 3 * num_points_;
+    parameters_ = new double[num_parameters_];
+
+    for (int i = 0; i < num_observations_; ++i) {
+        FscanfOrDie(fptr, "%d", camera_index_ + i);
+        FscanfOrDie(fptr, "%d", point_index_ + i);
+        for (int j = 0; j < 2; ++j) {
+            FscanfOrDie(fptr, "%lf", observations_ + 2 * i + j);
+        }
+    }
+
+    for (int i = 0; i < num_parameters_; ++i) {
+        FscanfOrDie(fptr, "%lf", parameters_ + i);
+    }
+
+    fclose(fptr);
+
+    use_quaternions_ = use_quaternions;
+    if (use_quaternions) {
+        // switch the angle-axis rotations to quaternions.
+        num_parameters_ = 10 * num_cameras_ + 3 * num_points_;
+        double *quaternion_parameters = new double[num_parameters_];
+        double *original_cursor = parameters_;
+        double *quaternion_cursor = quaternion_parameters;
+        for (int i = 0; i < num_cameras_; ++i) {
+            AngleAxisToQuaternion(original_cursor, quaternion_cursor);
+            quaternion_cursor += 4;
+            original_cursor += 3;
+            for (int j = 4; j < 10; ++j) {
+                *quaternion_cursor++ = *original_cursor++;
+            }
+        }
+        // copy the rest of the points.
+        for (int i = 0; i < 3 * num_points_; ++i) {
+            *quaternion_cursor++ = *original_cursor++;
+        }
+        // swap in the quaternion parameters.
+        delete[]parameters_;
+        parameters_ = quaternion_parameters;
+    }
+}
+
+void BALProblem::WriteToFile(const std::string &filename) const {
+    FILE *fptr = fopen(filename.c_str(), "w");
+
+    if (fptr == NULL) {
+        std::cerr << "Error: unable to open file " << filename;
+        return;
+    }
+
+    fprint(fptr, "%d %d %d %d\n", num_cameras_, num_cameras_, num_points_, num_observations_);
+
+    for (int i = 0; i < num_observations_; ++i) {
+        fprint(fptr, "%d %d", camera_index_[i], point_index_[i]);
+        for (int j = 0; j < 2; ++j) {
+            fprintf(fptr, " %g", observations_[2 * i + j]);
+        }
+        fprintf(fptr, "\n");
+    }
+
+    for (int i = 0; i < num_cameras(); ++i) {
+        double angleaxis[9];
+        if (use_quaternions_) {
+            //Output in angle_axis format.
+            QuaternionToAngleAxis(parameters_ + 10 * i, angleaxis);
+            memcpy(angleaxis + 3, parameters_ + 10 * i + 4, 6 * sizeof(double));
+        } else {
+            memcpy(angleaxis, parameters_ + 9 * i, 9 * sizeof(double));
+        }
+        for (int j = 0; j < 9; ++j) {
+            fprintf(fptr, "%.16g\n", angleaxis[j]);
+        }
+    }
+
+    const double *points = parameters_ + camera_block_size() * num_cameras_;
+    for (int i = 0; i < num_points(); ++i) {
+        const double *point = points + i * point_block_size();
+        for (int j = 0; j < point_block_size(); ++j) {
+            fprintf(fptr, "%.16g\n", point[j]);
+        }
+    }
+
+    fclose(fptr);
+}
+
+// Write the problem to a PLY file for inspection in Meshlab or CloudCompare
+void BALProblem::WriteToPLYFile(const std::string &filename) const {
+    std::ofstream of(filename.c_str());
+
+    of << "ply"
+       << '\n' << "format ascii 1.0"
+       << '\n' << "element vertex " << num_cameras_ + num_points_
+       << '\n' << "property float x"
+       << '\n' << "property float y"
+       << '\n' << "property float z"
+       << '\n' << "property uchar red"
+       << '\n' << "property uchar green"
+       << '\n' << "property uchar blue"
+       << '\n' << "end_header" << std::endl;
+
+    // Export extrinsic data (i.e., camera centers) as green points.
+    double angle_axis[3];
+    double center[3];
+    for (int i = 0; i < num_cameras(); ++i) {
+        const double *camera = cameras() + camera_block_size() * i;
+        CameraToAngleAxisAndCenter(camera, angle_axis, center);
+        of << center[0] << ' ' << center[1] << ' ' << center[2]
+           << " 0 255 0" << '\n';
+    }
+    of.close();
+}
+
+void BALProblem::CameraToAngleAxisAndCenter(const double *camera,
+                                            double *angle_axis,
+                                            double *center) const {
+    VectorRef angle_axis_ref(angle_axis, 3);
+    if (use_quaternions_) {
+        QuaternionToAngleAxis(camera, angle_axis);
+    } else {
+        angle_axis_ref = ConstVectorRef(camera, 3);
+    }
+
+    // c = -R't
+    Eigen::vectorXd inverse_roation = -angle_axis_ref;
+    AngleAxisRotatePoint(inverse_rotation.data(),
+                         camera + camera_block_size() - 6,
+                         center);
+    VectorRef(center, 3) *= -1.0;
+}
+
+void BALProblem::AngleAxisAndCenterToCamera(const double *angle_axis,
+                                            const double *center,
+                                            double *camera) const {
+    ConstVectorRef angle_axis_ref(angle_axis, 3);
+    if (use_quaternions_) {
+        AngleAxisToQuaternion(angle_axis, camera);
+    } else {
+        VectorRef(camera, 3) = angle_axis_ref;
+    }
+
+    // t = -R * c
+    AngleAxisRotatePoint(angle_axis, center, camera + camera_block_size() - 6);
+    VectorRef(camera + camera_block_size() - 6, 3) *= -1.0;
+}
+
+void BALProblem::Normalize() {
+    // compute the marginal median of the geometry
+    std::vector<double> tmp(num_points_);
+    Eigen::Vector3d median;
+    double *points = mutable_points();
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < num_points_; ++j) {
+            tmp[j] = points[3 * j + i];
+        }
+        median(i) = Median(&tmp);
+    }
+
+    //TODO
+}
